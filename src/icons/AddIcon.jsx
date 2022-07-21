8d2118b32eb10f4a25db887a09280db934e59116
Pablo Fabian <pablo.eth@hotmail.com>
2022-07-21 09:42:15 -0500

    add: icons

---
+++ src/icons/AddIcon.jsx
@@ -0,0 +1,17 @@
+export const AddIcon = (props) => (
+  <svg
+    width={24}
+    height={24}
+    strokeWidth={1.5}
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <path
+      d='M6 12h6m6 0h-6m0 0V6m0 6v6'
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+)
